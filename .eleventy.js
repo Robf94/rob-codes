501c462904e396d3f1037791595a8d9b0b2b6876
Robf94 <125037388+Robf94@users.noreply.github.com>
2023-12-11 11:16:33 +0000

    added subpages, fixed filepaths

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function(eleventyConfig) {
     // Set custom directories for input, output, includes, and data
+    eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addPassthroughCopy("./src/css/main.css");
     return {
       dir: {
         input: "src",

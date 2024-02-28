dea7ca9a5b96a8c0887794285502f5f44ebfbd04
Robf94 <125037388+Robf94@users.noreply.github.com>
2024-02-28 19:36:41 +0000

    added auto date to footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,19 @@
 module.exports = function(eleventyConfig) {
-    // Set custom directories for input, output, includes, and data
-    eleventyConfig.addPassthroughCopy("./src/assets");
-    eleventyConfig.addPassthroughCopy("./src/css/main.css");
-    return {
-      dir: {
-        input: "src",
-        includes: "_includes",
-        data: "_data",
-        output: "_site",
-      },
-      HTMLTemplateEngine: 'njk',
-    };
-   };
+  
+  // Set custom directories for input, output, includes, and data
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/css/main.css");
+
+  // Get current year NOTE lowercase "c" in "addShortcode" - took me about 30 minutes to realise this was throwing an error...
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+    HTMLTemplateEngine: 'njk',
+  };
+};
